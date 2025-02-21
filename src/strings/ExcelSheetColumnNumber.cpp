8f58921777eca13ecd17e14b235a5f011c6de4c5
khyzhun <sasha.khyzhun@gmail.com>
2025-02-21 23:54:34 +0200

    Solved: 168. Excel Sheet Column Title;

---
+++ src/strings/ExcelSheetColumnNumber.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+
+    
+        cout << "testing chars" << endl;
+        cout << 1 - 'a' << endl;
+
+        return 0;
+    }
+
+};
+
+int main() {
+    Solution().titleToNumber("asd");
+    return 0;
+}
